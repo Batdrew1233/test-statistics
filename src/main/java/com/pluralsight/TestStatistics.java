b7b9221d3c83a08703c3842d909fefc2ed7e7714
Batdrew1233 <drewstevenson1233@gmail.com>
2026-04-16 15:12:54 -0400

    Finished calculating sum

--- src/main/java/com/pluralsight/TestStatistics.java
+++ src/main/java/com/pluralsight/TestStatistics.java
@@ -1,4 +1,18 @@
 package com.pluralsight;
 
 public class TestStatistics {
+    public static void main(String[] args) {
+        //Test Score Array
+        int[] testScores = {90, 65, 84, 71, 96, 74 };
+
+        //variable to keep track of sum
+        int sum = 0;
+
+        //Goes through each test score and adds it to sum
+        for (int testScore : testScores) {
+            sum += testScore;
+
+        }
+        System.out.println("The Average is " + sum/ testScores.length + "%");
+    }
 }
